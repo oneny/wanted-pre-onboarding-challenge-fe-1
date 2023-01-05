b0827a9ccadae8ef5b61f4d45da64d7305e057aa
oneny <gjwodnjs1996@gmail.com>
2023-01-05 10:39:54 +0900

    feat: 로그인 페이지 Document Title 수정

--- client/src/pages/SigninPage/index.tsx
+++ client/src/pages/SigninPage/index.tsx
@@ -1,6 +1,8 @@
 import { SigninForm } from '@/components';
+import { useTitle } from '@/hooks/useTItle';
 
 export const SigninPage = () => {
+  useTitle('LOGIN - TODOS');
   return (
     <SigninForm />
   );
